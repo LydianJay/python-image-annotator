9ec25acebbdad02e5d8013ecbe2721b0edc7603e
LydianJay <lloydjayedradan@gmail.com>
2023-09-28 00:25:29 +0800

    add auto version

--- updator.py
+++ updator.py
@@ -6,6 +6,9 @@ def isUpToDate():
     response = requests.get(f'https://raw.githubusercontent.com/LydianJay/python-image-annotator/main/version.txt')
 
     if response.status_code == 200:
+        print(response.text.strip())
         return response.text.strip()
+    
     else:
+        print("Fail")
         return "FAIL"
